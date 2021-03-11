fe74545648119f932fd324c8b1910142b491d6b9
adiJiyer <janardhananaditya@gmail.com>
2021-03-11 21:06:32 +0530

    Application Configuration and common header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './screens/home/Home';
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    // <span>
+    //     Image Viewer
+    // </span>, 
+    <Home/>,
     document.getElementById('root')
 );
