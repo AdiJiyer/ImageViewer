7ef2f0ddea93359a78367462ce9868a7476c6bed
adiJiyer <janardhananaditya@gmail.com>
2021-03-15 20:57:51 +0530

    Test Data and linked app.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './screens/home/Home';
-import Login from './screens/login/Login'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    // <span>
-    //     Image Viewer
-    // </span>, 
-    //<Home/>
-    <Login/>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
     document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
